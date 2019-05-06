438e46c2ee374493e5bd98b8223c04e7a06d7c13
sunlijun <ashther@qq.com>
2019-05-06 18:53:54 +0800

    new proxy select method

--- R/rogue2.R
+++ R/rogue2.R
@@ -12,47 +12,54 @@ Rogue <- R6Class(
     iter_max = 10,
     is_record = FALSE,
     is_random = FALSE,
+    is_quite = FALSE,
 
     # initialize the object
     initialize = function(proxy = NULL, useragent = NULL, iter_max = 10,
-                          is_record = FALSE, is_random = FALSE) {
-      if (!'list' %in% class(proxy))
-        stop('proxy must be list class', call. = FALSE)
+                          is_record = FALSE, is_random = FALSE, is_quite = FALSE) {
+      if (!is.null(proxy)) {
+        if (!'list' %in% class(proxy))
+          stop('proxy must be list class', call. = FALSE)
+
+        self$proxy <- lapply(proxy, function(x) {
+          if (!all(c('ip', 'port') %in% names(x)))
+            stop('proxy must have ip and port at least', call. = FALSE)
+          if (!is.character(x$ip))
+            stop(
+              sprintf('ip must be character, not %s', typeof(x$ip)),
+              call. = FALSE
+            )
+          if (!is.numeric(x$port))
+            stop(
+              sprintf('port must be integer or numeric, not %s', typeof(x$port)),
+              call. = FALSE
+            )
+
+          if (!'times' %in% names(x))
+            x$times <- 1
+          if (!is.numeric(x$times))
+            stop(
+              sprintf('times must be integer or numeric, not %s', typeof(x$times)),
+              call. = FALSE
+            )
+
+          x
+        })
+      } else {
+        self$proxy <- proxy
+      }
+
       if (!is.numeric(iter_max))
         stop(
           sprintf('iter_max must be integer or numeric, not %s', typeof(iter_max)),
           call. = FALSE
         )
+      # TODO add warning when iter_max bigger than proxy length
       if (!is.logical(is_record))
         stop(
           sprintf('is_record must be logical, not %s', typeof(is_record)),
           call. = FALSE
         )
-
-      self$proxy <- lapply(proxy, function(x) {
-        if (!all(c('ip', 'port') %in% names(x)))
-          stop('proxy must have ip and port at least', call. = FALSE)
-        if (!is.character(x$ip))
-          stop(
-            sprintf('ip must be character, not %s', typeof(x$ip)),
-            call. = FALSE
-          )
-        if (!is.numeric(x$port))
-          stop(
-            sprintf('port must be integer or numeric, not %s', typeof(x$port)),
-            call. = FALSE
-          )
-
-        if (!'times' %in% names(x))
-          x$times <- 1
-        if (!is.numeric(x$times))
-          stop(
-            sprintf('times must be integer or numeric, not %s', typeof(x$times)),
-            call. = FALSE
-          )
-
-        x
-      })
 
       self$useragent <- useragent
       self$iter_max <- iter_max
@@ -63,14 +70,27 @@ Rogue <- R6Class(
     # print to console
     print = function(...) {
       cat('Rogue: \n')
-      cat(sprintf('  proxy: %s\n', length(self$proxy)))
-      cat(sprintf('  user agent: %s\n', length(self$useragent)))
+      cat(sprintf(
+        '  proxy: %s\n',
+        ifelse(is.null(self$proxy), 'NULL', length(self$proxy))
+      ))
+      cat(sprintf(
+        '  user agent: %s\n',
+        ifelse(is.null(self$useragent), 'NULL', length(self$useragent))
+      ))
       cat(sprintf('  iter max: %s\n', self$iter_max))
-      cat(sprintf('  record proxy history: %s', self$is_record))
+      cat(sprintf('  random select: %s\n', self$is_random))
+      cat(sprintf('  record history: %s\n', self$is_record))
+      cat(sprintf('  quite: %s\n', self$is_quite))
     },
 
     # add proxy by users anytime
     proxy_add = function(proxy, delete = FALSE) {
+
+    },
+
+    # show proxy with the tibble format
+    proxy_show = function(proxy) {
 
     },
 
@@ -99,11 +119,30 @@ Rogue <- R6Class(
     # all proxys including initializing and adding, must has elements with times name
     # if they don't have, must generate for them
     proxySelect = function() {
-      if (self$is_random)
-        return(sample(self$proxy, 1))
-      sample(
-        self$proxy, 1, prob = vapply(self$proxy, `[[`, 'times', FUN.VALUE = numeric(1))
-      )[[1]]
+      prob <- vapply(self$proxy, `[[`, 'times', FUN.VALUE = numeric(1))
+
+      if (self$iter_max <= length(self$proxy)) {
+        if (self$is_random) {
+          return(sample(self$proxy, self$iter_max))
+        } else {
+          return(sample(self$proxy, self$iter_max, prob = prob))
+        }
+      } else {
+        if (self$is_random) {
+          return(sample(self$proxy, self$iter_max, replace = TRUE))
+        } else {
+          modulus <- self$iter_max %/% length(self$proxy)
+          reminder <- self$iter_max %% length(self$proxy)
+
+          temp <- append(
+            replicate(
+              modulus, sample(self$proxy, length(self$proxy), prob = prob), FALSE
+            ),
+            list(sample(self$proxy, reminder, prob = prob))
+          )
+          return(unlist(temp, recursive = FALSE))
+        }
+      }
     },
 
     # confirm good proxy
@@ -168,11 +207,13 @@ Rogue <- R6Class(
       res_got <- 0
       iter <- 1
       parameters <- list(...)
+      if (!is.null(self$proxy))
+        proxy_selected <- private$proxySelect()
 
       proxy_bad <- list()
       while (iter <= self$iter_max) {
         if (!is.null(self$proxy)) {
-          proxy <- private$proxySelect()
+          proxy <- proxy_selected[[iter]]
           proxy_bad <- append(proxy_bad, list(proxy))
           parameters <- append(parameters, list(use_proxy(proxy$ip, proxy$port)))
         }
@@ -194,24 +235,24 @@ Rogue <- R6Class(
           }, proxy_bad)
           res_got <- 1
 
-        }, error = function(e) message(e$message))
+        }, error = function(e) {
+          if (!self$is_quite)
+            message(e$message)
+        })
 
         if (res_got == 1) break()
         iter <- iter + 1
       }
 
       if (res_got != 1 && iter > self$iter_max) {
-        if (is.null(parameters$quite)) { # TODO quite not work?
+        if (!self$is_quite)
           message('Try too many times.')
-        } else if (!parameters$quite) {
-          message('Try too many times.')
-        }
-        if (self$is_record)
+        if (!is.null(self$proxy) & self$is_record)
           private$proxyBadConfirm(proxy_bad)
         return(NULL)
       }
 
-      if (self$is_record) {
+      if (!is.null(self$proxy) & self$is_record) {
         private$proxyGoodConfirm(proxy_good)
         private$proxyBadConfirm(proxy_bad)
       }

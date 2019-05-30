54bf9ae366541f1dc93c93809ba93ce877b74e76
sunlijun <ashther@qq.com>
2019-05-30 18:46:57 +0800

    don't test deprecated function

--- tests/testthat/test-rogue.R
+++ tests/testthat/test-rogue.R
@@ -1,42 +1,5 @@
 context("test-request")
 
 test_that("request function", {
-  proxy_good <- list(list(ip = "221.206.100.133", port = 54781, times = 1))
-  user_agent_good <- "AdsBot-Google ( http://www.google.com/adsbot.html)"
 
-  temp <- request(
-    'GET', url = 'http://httpbin.org/get', .proxy = proxy_good, .user_agent = user_agent_good
-  )
-  expect_is(temp, 'list')
-  expect_equal(status_code(temp$response), 200)
-  expect_equal(temp$response$request$options$proxy, temp$proxy_good$ip)
-  expect_equal(temp$response$request$options$proxyport, temp$proxy_good$port)
-  expect_equal(temp$response$request$options$useragent, user_agent_good)
-
-  temp <- request(
-    'POST', url = 'http://httpbin.org/post', .proxy = proxy_good, .user_agent = user_agent_good
-  )
-  expect_is(temp, 'list')
-  expect_equal(status_code(temp$response), 200)
-  expect_equal(temp$response$request$options$proxy, temp$proxy_good$ip)
-  expect_equal(temp$response$request$options$proxyport, temp$proxy_good$port)
-  expect_equal(temp$response$request$options$useragent, user_agent_good)
-
-  temp <- request(
-    'PUT', url = 'http://httpbin.org/put', .proxy = proxy_good, .user_agent = user_agent_good
-  )
-  expect_is(temp, 'list')
-  expect_equal(status_code(temp$response), 200)
-  expect_equal(temp$response$request$options$proxy, temp$proxy_good$ip)
-  expect_equal(temp$response$request$options$proxyport, temp$proxy_good$port)
-  expect_equal(temp$response$request$options$useragent, user_agent_good)
-
-  temp <- request(
-    'DELETE', url = 'http://httpbin.org/delete', .proxy = proxy_good, .user_agent = user_agent_good
-  )
-  expect_is(temp, 'list')
-  expect_equal(status_code(temp$response), 200)
-  expect_equal(temp$response$request$options$proxy, temp$proxy_good$ip)
-  expect_equal(temp$response$request$options$proxyport, temp$proxy_good$port)
-  expect_equal(temp$response$request$options$useragent, user_agent_good)
 })
